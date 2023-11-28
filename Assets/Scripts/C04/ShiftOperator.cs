11d0135a8a62e0ee4008b6d033f93280a81f199e
Shrimp <tpwns0429@gmail.com>
2023-11-28 14:20:50 +0900

    BitwiseOperator 예제

--- Assets/Scripts/C04/ShiftOperator.cs
+++ Assets/Scripts/C04/ShiftOperator.cs
@@ -10,7 +10,9 @@ public class ShiftOperator : MonoBehaviour
 
         Debug.Log("Testing <<....");
 
-        int a = 1;
+        int a = 1; // 4byte 32bit공간에 뒤에가 1이되고 나머지는 0
+
+        // layer는 32bit로 구성되어있음
 
         Debug.Log(string.Format("a : {0:D5} (0x{0:X8})", a));
         Debug.Log(string.Format("a << 1 :{0:D5} (0x{0:X8})", a << 1));
@@ -31,7 +33,7 @@ public class ShiftOperator : MonoBehaviour
         int c = -255;
 
         Debug.Log(string.Format("c << : {0:D5} (0x{0:X8})", c));
-        Debug.Log(string.Format("c << 1 :{0:D5} (0x{0:X8})", c << 1));
+        Debug.Log(string.Format("c << 1 :{0:D5} (0x{0:X8})", c << 1)); //D는 10진수 == 첫번째 값은 10진수로 표현하는데 값을 5를넣어라
         Debug.Log(string.Format("c << 2: {0:D5} (0x{0:X8})", c << 2));
         Debug.Log(string.Format("c << 5: {0:D5} (0x{0:X8})", c << 5));
 
